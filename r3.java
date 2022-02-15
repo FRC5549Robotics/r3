efce57275cb1c57b4fc9a9bbe46d51a777bb2c6e
Gryphon5549 <firstteam5549@gmail.com>
2022-02-15 17:10:45 -0500

    Made replay template

--- r3.java
+++ r3.java
@@ -3,24 +3,23 @@ import java.lang.StringBuilder;
 import java.io.*
 
 public class Record {
-	public static final String logfile = "logfile.log";
+	public static String logfile = "logfile.log";
 	public static Record instance = null;
 	private FileOutputStream binout;
 	private ObjectOutputStream out;
 	private int start;
 
 	private Record() {
-		binout = new FileOutputStream(logfile);
-		obj = new ObjectOutputStream(binout);
+		obj = new ObjectOutputStream(new FileOutputStream(logfile));
 		start = System.currentTimeMillis();
 	}
 
 	private void recordCall(String function, Object... args) {
-		out.writeLong(System.currentTimeMillis());
-		out.writeChars(function);
+		out.writeLong(System.currentTimeMillis() - start);
+		out.writeObject(function);
 		out.writeInt(args.length);
 		for (Object o : args)
-			objout.writeObject(o);
+			out.writeObject(o);
 	}
 
 	public static Record getInstance() {
@@ -28,5 +27,24 @@ public class Record {
 			instance = new Record();
 		}
 		return instance;
+	}
+}
+
+public class Replay {
+	public static void replay(String logfile) throws IOException {
+		int start = System.currentTimeMillis();
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(logfile));
+		try {
+			while (true) {
+				long nextTime = in.readLong() + start;
+				String nextFunctionName = (String)in.readObject();
+				int numOfArgs = in.readInt();
+				Object[numOfArgs] args;
+				for (int i = 0; i < numOfArgs; i++)
+					args[i] = in.readObject();
+				Thread.sleep(nextTime - System.currentTimeMillis());
+				// Execute method
+			}
+		} catch (EOFError) {}
 	}
 }
