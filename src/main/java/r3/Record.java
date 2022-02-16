a879d816757c932e0f115f55b66d3bb0ab1b41aa
Pradhyum R <drpradhyum2016@outlook.com>
2022-02-16 02:12:39 +0000

    Improved API and wrote documentation

--- src/main/java/r3/Record.java
+++ src/main/java/r3/Record.java
@@ -1,30 +1,53 @@
 import java.io.*;
 
 public class Record {
-	public static String logfile = "logfile.log";
+	public static String recordingFile = "recording.bin";
 	public static Record instance = null;
 	public boolean corrupted = false;
 	private FileOutputStream binout;
 	private ObjectOutputStream out;
 	private long start;
+	private boolean recording = false;
 
 	private Record() throws IOException, FileNotFoundException {
-		out = new ObjectOutputStream(new FileOutputStream(logfile));
-		start = System.currentTimeMillis();
+		out = new ObjectOutputStream(new FileOutputStream(recordingFile));
 	}
 
-	private void recordCall(Signature sig, Object... args) {
-		try {
-			if (corrupted) throw new IOException();
-			out.writeLong(System.currentTimeMillis() - start);
-			out.writeObject(sig);
-			out.writeInt(args.length);
-			for (Object o : args)
-				out.writeObject(o);
-		} catch (IOException e) {
-			System.out.println("Corrupted.");
-			corrupted = true;
+	private void _start() {
+		start = System.currentTimeMillis();
+		recording = true;
+	}
+
+	public static void start() throws IOException, FileNotFoundException {
+		Record.getInstance()._start();
+	}
+
+	private void _recordCall(Signature sig, Object... args) {
+		if (!recording) {
+			try {
+				if (corrupted) throw new IOException();
+				out.writeLong(System.currentTimeMillis() - start);
+				out.writeObject(sig);
+				out.writeInt(args.length);
+				for (Object o : args)
+					out.writeObject(o);
+			} catch (IOException e) {
+				System.out.println("Corrupted.");
+				corrupted = true;
+			}
 		}
+	}
+
+	public static void recordCall(Signature sig, Object... args) throws IOException, FileNotFoundException {
+		Record.getInstance()._recordCall(sig, args);
+	}
+
+	private void _stop() {
+		recording = false;
+	}
+
+	public static void stop() throws IOException, FileNotFoundException {
+		Record.getInstance()._stop();
 	}
 
 	public static Record getInstance() throws IOException, FileNotFoundException {
